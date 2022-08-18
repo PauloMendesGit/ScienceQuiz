ec6784dfceb5654ccd2830502fddafd1b3375432
Paulo Mendes <paulomendes89@gmail.com>
2022-08-18 17:24:37 +0100

    Added basic Quiz UI and questions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sciencequiz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ScienceQuiz());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
