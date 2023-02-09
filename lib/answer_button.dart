2eabf1d65fd8f798bc91ab1d75d6ed07c648b925
Paulo Mendes <paumen@signicat.com>
2023-02-09 21:43:46 +0000

    Button functionality improvements
    changing button color
    disabling buttons after user interaction

---
+++ lib/answer_button.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'dart:async';
+
+class AnswerButton extends StatefulWidget {
+  final int answerNumber;
+
+  const AnswerButton({Key? key, required this.answerNumber}) : super(key: key);
+
+  @override
+  _AnswerButtonState createState() => _AnswerButtonState();
+}
+
+class _AnswerButtonState extends State<AnswerButton> {
+  final Color correctAnswerColor = const Color.fromRGBO(0, 190, 180, 1);
+  final Color wrongAnswerColor = const Color.fromRGBO(200, 55, 70, 1);
+  var currentButtonColor = Color.fromRGBO(32, 30, 63, 1);
+
+  bool checkAnswer(String userPickedAnswer) {
+    String correctAnswer = quizBrain.getCorrectAnswer();
+
+    print("Correct Answer: $correctAnswer");
+    print("User picked Answer: $userPickedAnswer");
+
+    if (userPickedAnswer == correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var defaultButtonColor = Color.fromRGBO(32, 30, 63, 1);
+    var correctAnswerColor = Color.fromRGBO(0, 190, 180, 1);
+    var wrongAnswerColor = Color.fromRGBO(200, 55, 70, 1);
+    var isCorrect = false;
+
+    return Expanded(
+        child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(currentButtonColor),
+                overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      return currentButtonColor;
+                    }),
+              ),
+              onPressed: () => setState(() {
+                isCorrect = checkAnswer(quizBrain.getQuestionOptions()[widget.answerNumber]);
+                print(isCorrect);
+                if (isCorrect) {
+                  currentButtonColor = correctAnswerColor;
+                } else {
+                  currentButtonColor = wrongAnswerColor;
+                }
+
+                Timer(const Duration(seconds: 2), () {
+                  print('Next Question Please');
+                  currentButtonColor = defaultButtonColor;
+                  if (quizBrain.isFinished()) {
+                    quizBrain.reset();
+                  } else {
+                    quizBrain.nextQuestion();
+                  }
+                  setState(() {});
+                });
+                setState(() {});
+              }),
+              child: Text(quizBrain.getQuestionOptions()[widget.answerNumber],
+                style: TextStyle(fontSize: 24),
+              ),
+            )));
+  }
+}
